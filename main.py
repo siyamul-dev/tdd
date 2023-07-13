efd5bc2b6fa31a1882d6885a3991f12f94374582
siyamul-dev <130224848+siyamul-dev@users.noreply.github.com>
2023-07-13 10:35:34 +0600

    back to correct version

--- main.py
+++ main.py
@@ -1,72 +1,49 @@
-from fastapi import FastAPI, File, UploadFile
-from fastapi.middleware.cors import CORSMiddleware
-import uvicorn
-import numpy as np
 from io import BytesIO
 from PIL import Image
 import tensorflow as tf
+
 app = FastAPI()
 
 # origins = [
 #     "http://localhost",
 #     "http://localhost:3000",
-#     "http://192.168.0.108:3000",
+#     "http://tdd.siyamul.com",
+#     "https://tdd.siyamul.com",
 # ]
 app.add_middleware(
     CORSMiddleware,
-    # allow_origins=origins,
     allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-# MODEL = tf.keras.models.load_model("./../models/2")
-MODEL = tf.keras.models.load_model("models/mango_v9.h5")
+MODEL = tf.keras.models.load_model("models/1")
 
-
-
-CLASS_NAMES = ['Anthracnose',
- 'Bacterial Canker',
- 'Cutting Weevil',
- 'Die Back',
- 'Gall Midge',
+CLASS_NAMES = ['Bacterial_spot',
+ 'Early_blight',
  'Healthy',
- 'Powdery Mildew',
- 'Sooty Mould']
+ 'Late_blight',
+ 'Leaf_Mold',
+ 'Mosaic_virus',
+ 'Septoria_leaf_spot',
+ 'YellowLeaf_Curl_Virus']
 
-@app.get("/")
-async def root():
-    return "Welcome! to 'Mango Leaf Diseases Prediction API'"
-    
 @app.get("/ping")
 async def ping():
-    return "Hello, I am alive"
-
-def read_file_as_image(data) -> np.ndarray:
-    image = np.array(Image.open(BytesIO(data)))
-    return image
-
-@app.post("/predict")
-async def predict(
+Expand All
+	@@ -46,8 +48,12 @@ async def predict(
     file: UploadFile = File(...)
 ):
     image = read_file_as_image(await file.read())
+    img_batch = np.expand_dims(image, 0)
 
-    image_resized = tf.image.resize(image, (224, 224))
-    img_batch = np.expand_dims(image_resized, 0)
-    # print(image.shape)
-    # print(image_resized.shape)
-   
     predictions = MODEL.predict(img_batch)
 
     predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
-    confidence = np.max(predictions[0])
-    return {
-        'class': predicted_class,
-        'confidence': float(confidence)
+Expand All
+	@@ -58,5 +64,5 @@ async def predict(
     }
 
 if __name__ == "__main__":
-    uvicorn.run(app, host='0.0.0.0', port=8000)
-
+    uvicorn.run(app, host='0.0.0.0', port=10000)
